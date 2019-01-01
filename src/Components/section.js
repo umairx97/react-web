2cbfc2c98983f7a422754eb76ff7da07a076b0d9
umairx97 <umair.bajwa@hotmail.com>
2019-01-01 22:03:13 +0500

    Adding Section

--- src/Components/section.js
+++ src/Components/section.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
+
 const Section = () => { 
     return ( 
         
-        <section class="intro">  
-        <div class="content">
-            <h1>You can create full screen sections without javascript.</h1>
-            <p>The height is set to 90vh, that means 90% height.</p>
-        </div>
-</section>
+//         <section class="intro">  
+//         <div class="content">
+//             <h1>You can create full screen sections without javascript.</h1>
+//             <p>The height is set to 90vh, that means 90% height.</p>
+//         </div>
+// </section>
+            <section id="intro">
+
+            <div class="content">
+               
+                    <h1 class="intro-header"  data-aos="fade-up">Download Our App Today!</h1>
+                    <p class="lead" data-aos="fade-up">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    </p>
+                    <ul class="download-badges">
+                        <a href="#" title="" className="style-btn btn-two">App Store</a>
+                    </ul>
+                </div>
+            </section>
     )
 }
 
